b3a64ef1082343f3f0763a42e571ffcb62e3dbc5
alvin <alvinsatria452@gmail.com>
2025-03-27 20:57:16 +0700

    lil progress

--- PemdasPraktikumModul4.java
+++ PemdasPraktikumModul4.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class PemdasPraktikumModul4 {
     public static void main(String[] args) {
-        
+        InformasiAkun info = new InformasiAkun();
+        Scanner input = new Scanner(System.in);
+        String nomorBca;
+
+        System.out.println("Masukkan nomor BCA anda: \n(3825180005 atau 5623300207 atau 7420400510)");
+        nomorBca = input.nextLine();
+        info.setNomorPelanggan(nomorBca);
+
     }
 }
