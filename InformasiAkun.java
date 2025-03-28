14447718544424dc6d8e5b5e2555758e2d830fcf
alvin <alvinsatria452@gmail.com>
2025-03-28 14:57:00 +0700

    progress

---
+++ InformasiAkun.java
@@ -0,0 +1,242 @@
+import java.util.Scanner;
+
+public class InformasiAkun {
+    Scanner input = new Scanner(System.in);
+
+    private int saldoUcil = 100000, saldoMiko = 200000, saldoJojo = 1000000;
+    private String nomorPelanggan;
+    private String nama;
+
+    public void setNomorPelanggan(String nomor){
+        nomorPelanggan = nomor;
+    }
+
+    public void checkNomorPelanggan(){
+        switch (nomorPelanggan) {
+            case "3825180005" -> {
+                setNama("Mang Ucil");
+
+                System.out.println("Selamat Siang " + nama + " :)\nIsi saldo anda adalah: Rp" + saldoUcil);
+                System.out.println("Masukkan jenis transaksi yang anda kehendaki:\n1. Pembelian\n2. Top up\n(masukkan angka 1 / 2):  ");
+                int niat = input.nextInt();
+
+                switch (niat) {
+                    case 1:
+                        System.out.println("Apa yang ingin anda beli:");
+                        System.out.println("1. Iphone 19 Pro Max Ultra [Rp30.000.000,00]");
+                        System.out.println("2. Kipas Angin Miko 3000rpm [Rp700.000,00]");
+                        System.out.println("3. Kotak Pensil Kalkulator otomatis [Rp100.000,00]");
+                        System.out.println("4. Literal mobil [Rp300.000.000,00]");
+                        System.out.println("5. Literal mobil tapi alphard [Rp1.000.000.000,00]");
+
+                        int beliApa = input.nextInt();
+                        switch (beliApa) {
+                            case 1:
+                                if (saldoUcil >= 30000000) {
+                                    saldoUcil -= 30000000;
+                                    System.out.println("Iphone 19 Pro Max Ultra Berhasil dibeli :D");
+                                    break;
+                                } else {
+                                    System.out.println("Uangmu kurang dawg!");
+                                    break;
+                                }
+                            case 2:
+                                if (saldoUcil >= 700000) {
+                                    saldoUcil -= 700000;
+                                    System.out.println("Kipas Angin Miko 3000rpm berhasil dibeli :D");
+                                    break;
+                                } else {
+                                    System.out.println("Uangmu kurang dawg!");
+                                    break;
+                                }
+                            case 3:
+                                if (saldoUcil >= 100000) {
+                                    saldoUcil -= 100000;
+                                    System.out.println("Kotak Pensil Kalkulator otomatis berhasil dibeli :D");
+                                    break;
+                                } else {
+                                    System.out.println("Uangmu kurang dawg!");
+                                    break;
+                                }
+                            case 4:
+                                if (saldoUcil >= 300000000) {
+                                    saldoUcil -= 300000000;
+                                    System.out.println("Literal mobil berhasil dibeli :D");
+                                    break;
+                                } else {
+                                    System.out.println("Uangmu kurang dawg!");
+                                    break;
+                                }
+                            case 5:
+                                if (saldoUcil >= 1000000000) {
+                                    saldoUcil -= 1000000000;
+                                    System.out.println("Literal mobil tapi alphard");
+                                    break;
+                                } else {
+                                    System.out.println("Uangmu kurang dawg!");
+                                    break;
+                                }
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Ingin topup berapa: ");
+                        int topup = input.nextInt();
+                        if (topup > 1000000000) {
+                            System.out.println("Maksimal topup adalah Rp1.000.000.000,00");
+                        } else {
+                            saldoUcil += topup;
+                            System.out.println("Anda berhasil topup sebesar " + topup);
+                            break;
+                        }
+                }
+
+            }
+            case "5623300207" -> {
+                setNama("Hatsune Miko");
+
+                System.out.println("Masukkan jenis transaksi yang anda kehendaki:\n1. Pembelian\n2. Top up\n(masukkan angka 1 / 2:  ");
+                int niat = input.nextInt();
+
+                switch (niat) {
+                    case 1:
+                        System.out.println("Apa yang ingin anda beli:");
+                        System.out.println("1. Iphone 19 Pro Max Ultra [Rp30.000.000,00]");
+                        System.out.println("2. Kipas Angin Miko 3000rpm [Rp700.000,00]");
+                        System.out.println("3. Kotak Pensil Kalkulator otomatis [Rp100.000,00]");
+                        System.out.println("4. Literal mobil [Rp300.000.000,00]");
+                        System.out.println("5. Literal mobil tapi alphard [Rp1.000.000.000,00]");
+
+                        int beliApa = input.nextInt();
+                        switch (beliApa) {
+                            case 1:
+                                if (saldoUcil >= 30000000) {
+                                    saldoUcil -= 30000000;
+                                    System.out.println("Iphone 19 Pro Max Ultra Berhasil dibeli :D");
+                                    break;
+                                } else {
+                                    System.out.println("Uangmu kurang dawg!");
+                                    break;
+                                }
+                            case 2:
+                                if (saldoUcil >= 700000) {
+                                    saldoUcil -= 700000;
+                                    System.out.println("Kipas Angin Miko 3000rpm berhasil dibeli :D");
+                                    break;
+                                } else {
+                                    System.out.println("Uangmu kurang dawg!");
+                                    break;
+                                }
+                            case 3:
+                                if (saldoUcil >= 100000) {
+                                    saldoUcil -= 100000;
+                                    System.out.println("Kotak Pensil Kalkulator otomatis berhasil dibeli :D");
+                                    break;
+                                } else {
+                                    System.out.println("Uangmu kurang dawg!");
+                                    break;
+                                }
+                            case 4:
+                                if (saldoUcil >= 300000000) {
+                                    saldoUcil -= 300000000;
+                                    System.out.println("Literal mobil berhasil dibeli :D");
+                                    break;
+                                } else {
+                                    System.out.println("Uangmu kurang dawg!");
+                                    break;
+                                }
+                            case 5:
+                                if (saldoUcil >= 1000000000) {
+                                    saldoUcil -= 1000000000;
+                                    System.out.println("Literal mobil tapi alphard");
+                                    break;
+                                } else {
+                                    System.out.println("Uangmu kurang dawg!");
+                                    break;
+                                }
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Ingin topup berapa: ");
+                        int topup = input.nextInt();
+                        saldoMiko += topup;
+                        System.out.println("Anda berhasil topup sebesar " + topup);
+                        break;
+                }
+
+            }
+            case "7420400510" -> {
+                setNama("Jonathan Joestar");
+
+                System.out.println("Masukkan jenis transaksi yang anda kehendaki:\n1. Pembelian\n2. Top up\n(masukkan angka 1 / 2:  ");
+                int niat = input.nextInt();
+
+                switch (niat) {
+                    case 1:
+                        System.out.println("Apa yang ingin anda beli:");
+                        System.out.println("1. Iphone 19 Pro Max Ultra [Rp30.000.000,00]");
+                        System.out.println("2. Kipas Angin Miko 3000rpm [Rp700.000,00]");
+                        System.out.println("3. Kotak Pensil Kalkulator otomatis [Rp100.000,00]");
+                        System.out.println("4. Literal mobil [Rp300.000.000,00]");
+                        System.out.println("5. Literal mobil tapi alphard [Rp1.000.000.000,00]");
+
+                        int beliApa = input.nextInt();
+                        switch (beliApa) {
+                            case 1:
+                                if (saldoUcil >= 30000000) {
+                                    saldoUcil -= 30000000;
+                                    break;
+                                } else {
+                                    System.out.println("Uangmu kurang dawg!");
+                                    break;
+                                }
+                            case 2:
+                                if (saldoUcil >= 700000) {
+                                    saldoUcil -= 700000;
+                                    break;
+                                } else {
+                                    System.out.println("Uangmu kurang dawg!");
+                                    break;
+                                }
+                            case 3:
+                                if (saldoUcil >= 100000) {
+                                    saldoUcil -= 100000;
+                                    break;
+                                } else {
+                                    System.out.println("Uangmu kurang dawg!");
+                                    break;
+                                }
+                            case 4:
+                                if (saldoUcil >= 300000000) {
+                                    saldoUcil -= 300000000;
+                                    break;
+                                } else {
+                                    System.out.println("Uangmu kurang dawg!");
+                                    break;
+                                }
+                            case 5:
+                                if (saldoUcil >= 1000000000) {
+                                    saldoUcil -= 1000000000;
+                                    break;
+                                } else {
+                                    System.out.println("Uangmu kurang dawg!");
+                                    break;
+                                }
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Ingin topup berapa: ");
+                        int topup = input.nextInt();
+                        saldoUcil += topup;
+                        System.out.println("Anda berhasil topup sebesar " + topup);
+                        break;
+                }
+
+            }
+            default -> System.out.println("\nNomor anda tidak terdaftar di server kami");
+        }
+    }
+
+    private void setNama(String nama){
+        this.nama = nama;
+    }
+}
--- PemdasPraktikumModul4.java
+++ PemdasPraktikumModul4.java
@@ -6,19 +6,38 @@ public class PemdasPraktikumModul4 {
         Scanner input = new Scanner(System.in);
         String nomorBca;
 
-        System.out.println("Masukkan nomor BCA anda: \n(3825180005 atau 5623300207 atau 7420400510)");
-        nomorBca = input.nextLine();
-        if (nomorBca.equals("3825180005")){
-            System.out.println("Masukkan Pin kahanda: ");
-            int pin = input.nextLine();
-            info.
-        } else if (nomorBca.equals("5623300207")){
-            System.out.println("Masukkan Pin kahanda: ");
-        } else if (nomorBca.equals("7420400510")){
-            System.out.println("Masukkan Pin kahanda: ");
-        } else {
-            System.out.println("\nTypo bruh, also masukkan nomor yang sudah disediakan aja");
+        while(true){
+            System.out.print("Masukkan nomor BCA anda: \n(3825180005 atau 5623300207 atau 7420400510)\n: ");
+            nomorBca = input.nextLine();
+            switch (nomorBca) {
+                case "3825180005":
+                {
+                    System.out.println("Masukkan Pin kahanda: ");
+                    String pin;
+                    pin = input.nextLine();
+                    if (pin.equals("200506")){
+                        info.setNomorPelanggan("3825180005");
+                        info.checkNomorPelanggan();
+                        break;
+                    } else {
+                        System.out.println("pin salahhhhh");
+                        break;
+                    }
+                }
+                case "5623300207":
+                    System.out.println("Masukkan Pin kahanda: ");
+                    break;
+                case "7420400510":
+                    System.out.println("Masukkan Pin kahanda: ");
+                    break;
+                case "exit":
+                    System.exit(0);
+                default:
+                    System.out.println("\nMaaf nomor anda tidak ada dalam database kami");
+                    break;
+            }
         }
+
 
     }
 }
